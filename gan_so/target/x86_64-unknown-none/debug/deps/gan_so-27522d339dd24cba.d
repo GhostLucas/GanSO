a9f3050141a67972a6dc349a9c4f995bfafdde48
GhostLucas <lucas_silva159@hotmail.com>
2023-02-05 10:54:30 -0300

    rustc targets bare metal

---
+++ gan_so/target/x86_64-unknown-none/debug/deps/gan_so-27522d339dd24cba.d
@@ -0,0 +1,5 @@
+/home/lucas/GanSO/gan_so/target/x86_64-unknown-none/debug/deps/gan_so-27522d339dd24cba: src/main.rs
+
+/home/lucas/GanSO/gan_so/target/x86_64-unknown-none/debug/deps/gan_so-27522d339dd24cba.d: src/main.rs
+
+src/main.rs:
